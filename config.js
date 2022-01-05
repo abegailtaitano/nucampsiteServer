1038a1bb9742867d6fc241787f671d58d085bedf
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-01-05 13:58:27 -0600

    FACEBOOK-PASSPORT-EXERCISE-COMPLETE

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '252183827044818',
+        clientSecret: '09fd3da66ae4e02e984'
+    }
 }
